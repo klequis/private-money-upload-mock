61457e4d7778a5ead3a5a2d5e702c99b6514fb95
klequis <klequis@gmail.com>
2021-05-06 17:24:28 -0700

    added acctId as key for dropzone

--- client/src/UploadFiles/DropZone.js
+++ client/src/UploadFiles/DropZone.js
@@ -2,7 +2,7 @@ import { DropDiv, DropMsgDiv, OnlyCSVMsgDiv } from './uploadFilesStyles'
 
 export const DropZone = ({ dropRef, getRootProps, getInputProps, account }) => {
   return (
-    <div className="card">
+    <div key={account.acctId} className="card">
       <div className="card-body">
         <h5 className="card-title">{account.acctName}</h5>
         <h6>x{account.acctNumber}</h6>
